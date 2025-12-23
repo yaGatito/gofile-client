5b6bd9b210a0f002470e821c66371a2b79585dd5
yaGatito <j.lazovskyi@gmail.com>
2025-12-23 07:35:12 +0200

    root constant shared

--- constants.go
+++ constants.go
@@ -1,9 +1,10 @@
 package gofile
 
 const (
-	postFolderEndpoint   = "https://api.gofile.io/contents/createFolder"
-	contentsEndpointPart = "https://api.gofile.io/contents/"
-	accountsEndpointPart = "https://api.gofile.io/accounts/"
-	getFileEndpoint      = "https://%s.gofile.io/download/web/%s/%s"
-	postFileEndpoint     = "https://upload.gofile.io/uploadfile"
+	RootFolder         = "root"
+	postFolderEndpoint = "https://api.gofile.io/contents/createFolder"
+	contentsBaseURL    = "https://api.gofile.io/contents/"
+	accountsBaseURL    = "https://api.gofile.io/accounts/"
+	getFileEndpoint    = "https://%s.gofile.io/download/web/%s/%s"
+	postFileEndpoint   = "https://upload.gofile.io/uploadfile"
 )
