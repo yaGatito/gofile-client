d2dde6157bb1675acefc66038f69242b8d8a93a2
yaGatito <j.lazovskyi@gmail.com>
2025-12-23 08:16:03 +0200

    examples fixed

--- examples/go.mod
+++ examples/go.mod
@@ -2,4 +2,4 @@ module main
 
 go 1.22
 
-require github.com/yaGatito/gofile-client v0.1.0
+require github.com/yaGatito/gofile-client v0.1.11
--- examples/main.go
+++ examples/main.go
@@ -3,97 +3,77 @@ package main
 import (
 	"context"
 	"io"
-	"net/http"
-
-	gfclient "github.com/yaGatito/gofile-client"
-
 	"log"
+	"net/http"
 	"os"
 	"time"
+	
+	"github.com/yaGatito/gofile-client"
 )
 
-// func main() {
-// 	ctx := context.Background()
-
-// 	client := gfclient.New("your-api-key", nil, nil)
-// 	if client == nil {
-// 		log.Fatal("Failed to create GoFile client, empty API key?")
-// 	}
-
-// 	file, err := os.Open("example.txt")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer file.Close()
-
-// 	resp, err := client.UploadFile(ctx, "root", "example.txt", file)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-
-// 	log.Printf("File uploaded: %+v", resp)
-// }
-
-var apiKey string
-var folderId string
-
-func init() {
-	apiKey = os.Getenv("GOFILE_API_KEY")
-	folderId = os.Getenv("GOFILE_ACCOUNT_ID")
-
-	if apiKey == "" || folderId == "" {
-		log.Fatal("GOFILE_API_KEY and GOFILE_ACCOUNT_ID environment variables must be set")
-	}
+func main() {
+	usecase()
 }
 
-func main() {
-	// A simple use case example.
-	logger := log.New(os.Stdout, "[GOFILE-CLIENT] ", log.Ldate|log.Ltime|log.Lmicroseconds|log.LUTC)
-	client, err := gfclient.New(apiKey, &http.Client{Timeout: 15 * time.Second}, logger)
-	if err != nil {
-		logger.Fatal("failed to create client:", err)
+func usecase() {
+	apiKey := os.Getenv("GOFILE_API_KEY")
+	if apiKey == "" {
+		log.Fatal("GOFILE_API_KEY environment variable must be set")
 	}
-	ctx := context.Background()
 
-	// Creating a "NewFolder" folder.
-	createFolderResponse, err := client.CreateFolder(ctx, "root", "NewFolder")
+	// A simple use case example.
+	loggerTag := "[GOFILE-CLIENT] "
+	logger := log.New(os.Stdout, loggerTag, log.Ldate|log.Ltime|log.Lmicroseconds|log.LUTC)
+	client, err := gofile.New(apiKey, &http.Client{Timeout: 15 * time.Second}, logger)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("Failed to create client:", err)
 	}
-	logger.Println("Response received:", createFolderResponse)
 
 	// Setting context with timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	// Opening file
-	file, err := os.Open("./files/file.txt")
+	// Creating a "NewFolder" folder.
+	folderName := "NewFolder"
+	createFolderResponse, err := client.CreateFolder(ctx, gofile.RootFolder, folderName)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("Failed to create folder:", err)
+	}
+	logger.Println("Response received:", createFolderResponse)
+
+	// Opening file
+	fileName := "sample.txt"
+	file, err := os.Open("./" + fileName)
+	if err != nil {
+		logger.Fatal("Failed to create folder:", err)
 	}
 
 	// Uploading file
-	postFileResponse, err := client.UploadFile(ctx, createFolderResponse.Data.Id, "./files/file.txt", file)
+	postFileResponse, err := client.UploadFile(ctx, createFolderResponse.Data.Id, fileName, file)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("Failed to upload file:", err)
 	}
 	logger.Println("UploadFileResponseBody received:", postFileResponse)
 
 	// Getting file details in order to download it later
 	getFileInfoResponse, err := client.GetFileInfo(ctx, postFileResponse.Data.Id)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("Failed to retrieve file details:", err)
 	}
 	logger.Println("GetFileInfoResponseBody received:", getFileInfoResponse)
 
 	// Getting file's io.ReadCloser from response
 	reader, err := client.DownloadFile(ctx, getFileInfoResponse.Data.ServerSelected, getFileInfoResponse.Data.Id, getFileInfoResponse.Data.Name)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal("Failed to download file:", err)
 	}
 	defer reader.Close()
+
 	bytes, err := io.ReadAll(reader)
+	if err != nil {
+		logger.Fatal("Failed to read file from response:", err)
+	}
 
 	logger.Println("File get:", len(bytes), "bytes")
-	logger.Println("File string content:", string(bytes))
+	logger.Println("File content:", string(bytes))
 }
