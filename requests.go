b1e5c3024814f8cc0c279737dcbc52b7047e3759
yaGatito <j.lazovskyi@gmail.com>
2025-12-23 07:46:57 +0200

    constant usages

--- requests.go
+++ requests.go
@@ -10,7 +10,7 @@ import (
 // createGetFileInfoRequest builds an HTTP GET request for retrieving
 // detailed metadata for the specified file ID.
 func (c *GofileClient) createGetFileInfoRequest(ctx context.Context, fileId string) (*http.Request, error) {
-	url := fmt.Sprintf("%s%s", contentsEndpointPart, url.PathEscape(fileId))
+	url := fmt.Sprintf("%s%s", contentsBaseURL, url.PathEscape(fileId))
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	req.Header.Set("X-Website-Token", "4fd6sg89d7s6")
 	if err != nil {
@@ -22,7 +22,7 @@ func (c *GofileClient) createGetFileInfoRequest(ctx context.Context, fileId stri
 // createGetIdRequest builds an HTTP GET request for retrieving
 // the account ID associated with the API key in use.
 func (c *GofileClient) createGetIdRequest(ctx context.Context) (*http.Request, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, accountsEndpointPart+"getid", nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, accountsBaseURL+"getid", nil)
 	if err != nil {
 		return nil, fmt.Errorf("creating 'getid' request: %w", err)
 	}
@@ -33,7 +33,7 @@ func (c *GofileClient) createGetIdRequest(ctx context.Context) (*http.Request, e
 // createGetAccountInfoRequest builds an HTTP GET request for retrieving
 // account metadata for the specified account ID.
 func (c *GofileClient) createGetAccountInfoRequest(ctx context.Context, accountId string) (*http.Request, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, accountsEndpointPart+accountId, nil)
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, accountsBaseURL+accountId, nil)
 	if err != nil {
 		return nil, fmt.Errorf("creating 'getAccountInfo' request: %w", err)
 	}
